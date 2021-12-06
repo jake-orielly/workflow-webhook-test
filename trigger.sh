6d48887c98f801f591525fe1200b8676309c8f71
Jake O'Rielly <jake.orielly@gmail.com>
2021-12-05 21:27:02 -0800

    Workflow update 1

---
+++ trigger.sh
@@ -0,0 +1,4 @@
+curl -H "Authorization: token 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" \
+	    --request POST \
+	        --data '{"event_type": "webhook"}' \
+		    https://api.github.com/repos/jake-orielly/workflow-webhook-test/dispatches
